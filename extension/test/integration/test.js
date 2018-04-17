2756f2b6672beda7a50e247934ce72f59ebefdfb
Oliver Müller <oliver.mueller@shopgate.com>
2018-04-17 10:50:16 +0200

    Initial commit

---
+++ extension/test/integration/test.js
@@ -0,0 +1,7 @@
+const assert = require('assert')
+
+describe('Extension', () => {
+  it('should have an integration test', () => {
+    assert.equal(true, true)
+  });
+});
---
+++ extension/test/unit/test.js
@@ -0,0 +1,7 @@
+const assert = require('assert')
+
+describe('Extension', () => {
+  it('should have an unit test', () => {
+    assert.equal(true, true)
+  });
+});
