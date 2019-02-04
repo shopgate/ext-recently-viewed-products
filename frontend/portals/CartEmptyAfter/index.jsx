775ea23d9d539599a6b4ee3fce41f2acfc2d279c
Szymon Nowicki <sznowicki@eskalacja.com>
2019-02-04 10:04:58 +0100

    CCP-1151 - missing jsdocs

--- frontend/portals/CartEmptyAfter/index.jsx
+++ frontend/portals/CartEmptyAfter/index.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ProductsSlider from '../../components/ProductSlider';
 import { getRecentlyViewedProductsWithLimit, hasMore } from '../../selectors';
 
 /**
  * Portal position for Products Slider on PDP.
+ * @params {Array} products Products collection.
+ * @params {bool} showMore Whether to show more button (products.length > totalCount)
  * @returns {JSX}
  */
-const CartEmptyAfter = ({ products, showMore }) => {
-  return (
-    <ProductsSlider
-      isCartPage
-      showMore={showMore}
-      products={products}
-    />
-  );
+const CartEmptyAfter = ({ products, showMore }) => (
+  <ProductsSlider
+    isCartPage
+    showMore={showMore}
+    products={products}
+  />
+);
+
+CartEmptyAfter.propTypes = {
+  products: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  showMore: PropTypes.bool.isRequired,
 };
 
+/**
+ * Maps state to props.
+ * @param {Object} state State.
+ * @returns {Object}
+ */
 const mapStateToProps = state => ({
   products: getRecentlyViewedProductsWithLimit(state),
   showMore: hasMore(state),
