ed7f7b6ced741b4d5566c87295f21505d056284e
aylinuenal <aylin.uenal@shopgate.com>
2025-06-23 16:23:14 +0200

    CURB-5127 remove helper function and refactor empty cart

--- frontend/components/EmptyCart/index.jsx
+++ frontend/components/EmptyCart/index.jsx
@@ -1,33 +1,41 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import I18n from '@shopgate/pwa-common/components/I18n';
-import { themeConfig } from '@shopgate/pwa-common/helpers/config';
-import { encodeSVG } from '@shopgate/engage/core/helpers';
+import { themeConfig } from '@shopgate/engage';
+import PropTypes from 'prop-types';
 import Icon from './components/Icon';
 import styles from './style';
 
-const { fullSVGs = {} } = themeConfig || {};
+const { svgImages = {} } = themeConfig || {};
 
 /**
  * The Cart Empty component.
  * If configured, theme config will override the icon here
+ * @param {React.node} children children to display if image is configured in theme config
  * @return {JSX.Element}
  */
-const Empty = () => {
-  const { emptyCart = '' } = fullSVGs || {};
-
-  const imageSRC = useMemo(() => encodeSVG(emptyCart),
-    [emptyCart]);
+const Empty = ({ children }) => {
+  const { emptyCart = '' } = svgImages || {};
 
   return (
     <div className={styles.container}>
-      <div className={imageSRC ? styles.image : styles.icon} aria-hidden>
-        {imageSRC ? <img src={imageSRC} alt="" /> : <Icon />}
+      <div className={emptyCart ? styles.image : styles.icon} aria-hidden>
+        {emptyCart ? children : <Icon />}
       </div>
+      {!emptyCart && (
       <div className={styles.title}>
         <I18n.Text string="cart.empty" />
       </div>
+      )}
     </div>
   );
+};
+
+Empty.propTypes = {
+  children: PropTypes.node,
+};
+
+Empty.defaultProps = {
+  children: null,
 };
 
 export default Empty;
--- frontend/components/NoProducts/index.jsx
+++ frontend/components/NoProducts/index.jsx
@@ -1,27 +1,15 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import I18n from '@shopgate/pwa-common/components/I18n';
-import { themeConfig } from '@shopgate/pwa-common/helpers/config';
-import { encodeSVG } from '@shopgate/engage/core/helpers';
 import styles from './style';
-
-const { fullSVGs } = themeConfig;
 
 /**
  * No products component
  * @returns {JSX.Element}
  */
-const NoProducts = () => {
-  const { noResultsImage } = fullSVGs;
-
-  const imageSRC = useMemo(() => encodeSVG(noResultsImage),
-    [noResultsImage]);
-
-  return (
-    <div className={styles.wrapper}>
-      {noResultsImage && <img src={imageSRC} alt="" />}
-      <I18n.Text string="recently_viewed_products.empty_product_list" />
-    </div>
-  );
-};
+const NoProducts = () => (
+  <div className={styles.wrapper}>
+    <I18n.Text string="recently_viewed_products.empty_product_list" />
+  </div>
+);
 
 export default NoProducts;
