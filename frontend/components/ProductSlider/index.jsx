c5502a72cfb07c6e15445c62c37ff79f79f71d6a
aylinuenal <aylin.uenal@shopgate.com>
2025-06-23 09:27:04 +0200

    CURB-5128 adjust class name for slider headline

--- frontend/components/ProductSlider/index.jsx
+++ frontend/components/ProductSlider/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useTheme } from '@shopgate/engage/core';
 import { I18n, ButtonLink } from '@shopgate/engage/components';
+import { ProductSlider as BaseProductSlider } from '@shopgate/engage/product/components';
 import getConfig from '../../helpers/getConfig';
 import connect from './connector';
 import styles from './style';
@@ -29,8 +29,6 @@ const ProductSlider = ({
   headline,
   className,
 }) => {
-  const { ProductSlider: BaseProductSlider } = useTheme();
-
   if (isCartPage && !showOnEmptyCartPage) {
     return null;
   }
@@ -54,7 +52,7 @@ const ProductSlider = ({
   return (
     <div className={`${styles.slider} ${className}`}>
       <div className={styles.headlineContainer}>
-        <h3 className={`${styles.headline(hasShowMore)} headline`}>
+        <h3 className={`${styles.headline(hasShowMore)} recently-viewed-products__product-slider__headline`}>
           <I18n.Text string={headline || defaultHeadline} />
         </h3>
         {hasShowMore && (
