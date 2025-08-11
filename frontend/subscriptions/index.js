37b2af44531811196d6cece14a56a886e423bf1a
Sascha Krist <sascha.krist@shopgate.com>
2025-08-11 14:48:52 +0200

    CURB-5289 small fix

--- frontend/subscriptions/index.js
+++ frontend/subscriptions/index.js
@@ -4,9 +4,10 @@ import { ITEM_PATTERN } from '@shopgate/pwa-common-commerce/product/constants';
 import { ACTION_PUSH } from '@virtuous/conductor';
 import { hex2bin } from '@shopgate/pwa-common/helpers/data';
 import { getBaseProductId } from '@shopgate/pwa-common-commerce/product/selectors/product';
-import {
-  sendDefaultLocationCodeSuccess$,
-} from '@shopgate/engage/locations';
+// import {
+//   sendDefaultLocationCodeSuccess$,
+// } from '@shopgate/engage/locations';
+import { productDataExpired$ } from '@shopgate/engage/product';
 import {
   addRecentlyViewedProducts,
   fetchRecentlyViewedProducts,
@@ -48,8 +49,8 @@ export default function recentlyViewedProducts(subscribe) {
     dispatch(fetchRecentlyViewedProducts());
   });
 
-  // Expire the recently viewed products when location is updated
-  subscribe(sendDefaultLocationCodeSuccess$, ({ dispatch }) => {
+  // Fetch recently viewed products when product data was expired
+  subscribe(productDataExpired$, ({ dispatch }) => {
     dispatch(fetchRecentlyViewedProducts());
   });
 }
